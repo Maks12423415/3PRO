8cee66cb8d68427e7c3d6a20de42547bce6ae706
Maks Samborski <makxc7@gmail.com>
2024-09-20 20:52:52 +0200

    New tasks

--- Next.js/my-app/src/app/page.js
+++ Next.js/my-app/src/app/page.js
@@ -1,28 +1,21 @@
-// Importujemy komponent Link z Next.js, który służy do nawigacji między stronami
-import Link from "next/link";
+// Importy z Next.js i komponentów z aplikacji
+import Link from "next/link"; // Komponent Link do nawigacji między stronami
 
-// Importujemy nasz komponent ItemList, który prawdopodobnie renderuje listę elementów
+// Importy komponentów z lokalnych plików
 import ItemList from "../components/ItemList";
-
-// Importujemy style CSS z lokalnego pliku modułowego
-import styles from "./index.module.css";
-
-// Importujemy nasz komponent TextBlock, który znajduje się w pliku Footer.js
 import TextBlock from "../components/TextBlock";
-
-import Footer from "../components/Footer"; // Import komponentu Footer
-
+import Footer from "../components/Footer";
 import ImageWithText from "../components/ImageWithText";
-
 import SimpleForm from "@/components/SimpleForm";
-
 import ConditionalDisplay from "@/components/ConditionalDisplay";
-
 import ListWithObjects from "@/components/ListWithObjects";
+
+// Importy stylów CSS
+import styles from "./index.module.css"; // Stylowanie dla tego komponentu
 
 // Eksportujemy funkcję HomePage jako domyślny eksport tego modułu
 export default function HomePage() {
-  // Definiujemy tablicę z ulubionymi filmami
+  // Definiujemy tablicę z danymi do komponentów
   const favoriteMovies = ["film1", "film2", "film3"];
   const imageUrl =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3vrTUU3CKbUDThpm8aZzFXdTmai6PodNfXA&s";
@@ -34,25 +27,39 @@ export default function HomePage() {
   // Renderujemy stronę główną
   return (
     <div>
-      {/* Nagłówek z klasą CSS z modułowego pliku stylów */}
+      {/* Sekcja z komponentem TextBlock, który pokazuje nagłówek i treść */}
       <TextBlock
         heading="Informacje o stronie"
         content="Jest to strona o mnie!"
       />
+
+      {/* Wyświetlamy listę ulubionych filmów */}
       <h2>Ulubione filmy:</h2>
-      {/* Wywołujemy komponent ItemList, przekazując mu tablicę ulubionych filmów */}
       <ItemList items={favoriteMovies} />
-      {/* Link do strony "about", który pozwala na nawigację do innej strony w aplikacji */}
-      <Link href="/about">Moje hobby</Link> <br />
-      <Link href="/contact">Kontakt</Link>
-      <br />
-      <Link href="/o_nas">O nas</Link>
-      <ImageWithText imageUrl={imageUrl} text={"Pies"} />
-      {/* Wywołujemy komponent Footer */}
-      <Footer />
-      <SimpleForm placeholder="Podaj imie..." value="text" label="Imie: " />
+
+      {/* Sekcja nawigacji z linkami do innych stron */}
+      <nav>
+        <Link href="/about">Moje hobby</Link> <br />
+        <Link href="/contact">Kontakt</Link> <br />
+        <Link href="/o_nas">O nas</Link>
+      </nav>
+
+      {/* Wyświetlenie obrazka z tekstem przy użyciu komponentu ImageWithText */}
+      <ImageWithText imageUrl={imageUrl} text="Pies" />
+
+      {/* Komponent formularza SimpleForm */}
+      <SimpleForm placeholder="Podaj imię..." label="Imię: " />
+
+      {/* Komponent ConditionalDisplay z warunkowym renderowaniem */}
       <ConditionalDisplay isVisible={true} />
+
+      {/* Wyświetlenie listy z obiektami */}
       <ListWithObjects items={table} />
+
+      {/* Sekcja z komponentem Footer */}
+      <Footer />
+
+      {/* Dodanie autora strony */}
       <h3>Wykonawca: Maks Samborski</h3>
     </div>
   );
