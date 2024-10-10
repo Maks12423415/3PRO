8535374ccb9887f1710327d3fbed4d472acbe4b6
Maks Samborski <makxc7@gmail.com>
2024-10-10 12:26:02 +0200

    RestCountries - details

--- next_v2/src/components/CountryBorder.js
+++ next_v2/src/components/CountryBorder.js
@@ -7,6 +7,8 @@ export default function CountryBorder({countrys}){
 
     const [countries, setCountries] = useState(null)
     const [error, setError] = useState(false)
+    const [minPopilation, setMinPopulation] = useState(0)
+    const [maxPopulation, setMaxPopulation] = useState(0)
 
     useEffect(() => {
 const getData = async ()=>{
@@ -20,8 +22,10 @@ for(const country of countrys){
         const Json = await response.json();
         countriesData.push(Json[0])
 }
+    
+    setMinPopulation(Math.min(...countriesData.map(country=>country.population)))
+    setMaxPopulation(Math.max(...countriesData.map(country=>country.population)))
     setCountries(countriesData)
-
     
 }catch(error){
     setError(true)
@@ -33,14 +37,16 @@ for(const country of countrys){
 
 }, [countrys])
 
+
+
     return(
         <>
 
             {error && <h1>Nie udało się pobrać danych</h1>}
 
             {countries != null && countries.map((border, index)=>
-                <Link key={index} href={`/RestCountries/${border.cca2}`}>
-                    <Country key={index} kraj={border} />
+                <Link key={index} href={`/RestCountries/${border.cca2}`} className={`${minPopilation === countries.population ? "border-red-500" : maxPopulation ? "border-green-500": "" }`}>
+                    <Country  kraj={border} />
                 </Link>
             )} 
 
