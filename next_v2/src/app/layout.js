8ac133fb1fe8f7cc98955509f8a4e19c3853d955
Maks Samborski <makxc7@gmail.com>
2024-10-23 19:05:45 +0200

    .

--- next_v2/src/app/layout.js
+++ next_v2/src/app/layout.js
@@ -2,9 +2,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
-import { Label } from "@/components/ui/label";
-import { Switch } from "@/components/ui/switch";
-import { useState } from "react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,22 +16,12 @@ const inter = Inter({
 
 // Główna struktura layoutu aplikacji
 export default function RootLayout({ children }) {
-  const [theme, setTheme] = useState("bg-white text-black");
-
-  function color() {
-    setTheme(
-      theme === "bg-white text-black"
-        ? "bg-black text-white"
-        : "bg-white text-black"
-    );
-  }
   return (
     <html lang="en">
-      <body className={`${inter.className} ${theme}`}>
+      <body className={`${inter.className} `}>
         {/* Nawigacja na każdej stronie */}
         <NavBar />
-        <Switch onClick={color} id="airplane-mode" />
-        <Label htmlFor="airplane-mode">Mood</Label>
+
         {children}
       </body>
     </html>
